71c3c142d5b98d9c66b62b1d555f4621697c0dd9
Khổng Thị Ngọc Mai <NgocMai@Khongs-MBP.fritz.box>
2017-06-06 16:13:47 +0200

    Solved 1.1

--- K1.1/Program.cs
+++ K1.1/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace K1
 {
     class MainClass
     {
+        public static bool Unique(string a)
+        {
+            for (int i = 0; i < a.Length; ++i)
+            {
+                for (int j = i + 1; j < a.Length; ++j)
+                {
+                    if (a[i] == a[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("gib String ein");
+            string a = Console.ReadLine();
+            if (Unique(a)) {
+                Console.WriteLine("Unique");
+            }
+            else {Console.WriteLine("nicht unique");}
+            Console.ReadKey();
         }
     }
 }
