e459e916e44a303069ab2b740a36f8588eb7b617
Khổng Thị Ngọc Mai <NgocMai@Khongs-MBP.fritz.box>
2017-06-06 21:21:43 +0200

    Solved 1.5

---
+++ K1.1/1_5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+namespace K1
+{
+    public class __5
+    {
+        public static string Compression(string a)
+        {
+            StringBuilder mystring = new StringBuilder();
+            for (int i = 0; i < a.Length; ++i)
+            {
+                int e = 1;
+                while ((i+1<a.Length)&&(a[i] == a[i + 1]))
+                {
+                    ++e;
+                    ++i;
+			    }
+                mystring.Append(a[i]);
+                mystring.Append(e.ToString());
+            }
+            if (mystring.ToString().Length > a.Length)
+                       return a;
+            else return mystring.ToString();
+        }
+        public static void Main()
+        {
+            Console.WriteLine("string");
+            string a = Console.ReadLine();
+            Console.WriteLine(Compression(a));
+            Console.ReadKey();
+        }
+    }
+}
