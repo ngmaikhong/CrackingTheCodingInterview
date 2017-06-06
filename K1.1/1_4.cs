aa88e4db25dba42871da5853bd82a7b893bd56e1
Khổng Thị Ngọc Mai <NgocMai@Khongs-MBP.fritz.box>
2017-06-06 19:56:09 +0200

    Solved 1.4

---
+++ K1.1/1_4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+namespace K1
+{
+    public class __4
+    {
+        public static StringBuilder Replace(string a)
+        {
+            StringBuilder mystring = new StringBuilder(a);
+            if (a.Contains(" "))
+            {
+                mystring.Replace(" ", "%20");
+            }
+            return mystring;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("string");
+            string a = Console.ReadLine();
+            Console.WriteLine(Replace(a).ToString());
+            Console.ReadKey();
+        }
+    }
+}
