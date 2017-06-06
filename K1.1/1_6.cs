e04d93bb6adb339dade4d264a8df8681d378d682
Khổng Thị Ngọc Mai <NgocMai@Khongs-MBP.fritz.box>
2017-06-06 22:24:13 +0200

    1.6 Solved#

---
+++ K1.1/1_6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace K1
+{
+    public class __6
+    {
+        public static int[,] Rotate(int[,] matrix)
+        {
+            int[,] ergebnis = new int[3, 3];
+            for (int i = matrix.GetLength(0) - 1; i >= 0; --i)
+            {
+                for (int j = 0; j < matrix.GetLength(1); ++j)
+                {
+                    ergebnis[i, j] = matrix[matrix.GetLength(0) - 1 - j, i];
+                }
+            }
+            return ergebnis;
+        }
+        public static void Main()
+        {
+            int[,] matrix = new int[3, 3] { { 4, 5, 6 }, { 7, 8, 9 }, { 2, 3, 4 } };
+            int[,] ergebnis = Rotate(matrix);
+
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    Console.Write(ergebnis[i, j]);
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
